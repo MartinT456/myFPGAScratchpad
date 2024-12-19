7a9f47984a879757cae8be313da49aceb823a780
MartinT456 <martinqt456@gmail.com>
2024-12-19 13:39:42 -0800

    Created UART TX module

---
+++ UART_COMD_A735T/UART_COMD_A735T.srcs/sources_1/new/UART_RRX_COMD_A735T.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/19/2024 12:25:33 PM
+// Design Name: 
+// Module Name: UART_TX_COMD_A735T
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_TX_COMD_A735T(
+
+    );
+endmodule
